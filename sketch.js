ebf4fc7fa56d27543b2133627651578b40654433
alefac <facchin.alessandra@gmail.com>
2018-11-05 10:24:51 +0100

    slitscan

--- sketch.js
+++ sketch.js
@@ -1,11 +1,54 @@
-function preload(){
-  // put preload code here
+// Global variables
+var video;
+var w = 720;
+var h = 480;
+var inc = 0;
+var button;
+
+function preload() {
+
 }
 
 function setup() {
-  // put setup code here
+  // Canvas
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  // Declaration of the density of the pixels
+  pixelDensity(1);
+  // Title
+  fill('blue');
+  textFont('Karla');
+  textSize(40);
+  textStyle(BOLD);
+  text('Slitscan', 20, 50);
+  // Text
+  fill(0);
+  textSize(20);
+  textStyle(NORMAL);
+  text('Move your head in front of the camera and save the result, if you want :)', 20, 80);
+  // Webcam input
+  video = createCapture(VIDEO);
+  video.size(w, h);
+  // Save button
+  button = createButton('Save Picture');
+  button.position((width / 2) - (w / 2), (height / 2) + (h / 2) + 20);
+  button.mousePressed(savePic);
 }
 
+
 function draw() {
-  // put drawing code here
+  // Slitscan video position variables
+  var x = (width / 2) - (w / 2);
+  var y = (height / 2) - (h / 2);
+  // Slitscan
+  copy(video, w / 2, 0, 1, h, x + inc, y, 1, h);
+  inc++;
+  if (inc > w) {
+    inc = 0;
+  }
+}
+
+// Save picture function
+function savePic() {
+  saveCanvas('slitScan', 'jpg');
 }
